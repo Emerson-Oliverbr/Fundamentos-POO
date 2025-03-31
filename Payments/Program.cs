4b57cbbad8a9810d910960bd61da86d2cb5a0601
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-03-31 20:36:24 -0300

    Iniciando o curso

---
+++ Payments/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Payments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mundo!");            
+        }       
+    }
+}
+
